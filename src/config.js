90f5d8738a7f0b98057c4dfebec89f92feaf3cc2
tenado <690784374@qq.com>
2024-04-29 15:09:22 +0800

    1

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const default_config = {
+  
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * 解析html字符串
+ * 
+*/
+export const parse = (html, options = {}) => {}
+
+/**
+ * 解析html片段
+ * 
+*/
+export const parseFragment = (html, options = {}) => {}
