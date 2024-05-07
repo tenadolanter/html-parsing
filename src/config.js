a3bc3e9beef1ee5ef52cb615c2e2c52d30550f17
tenado <690784374@qq.com>
2024-05-07 15:31:01 +0800

    1

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
 import TreeAdapter from "./adapter/index.js";
 const default_config = {
   // 是否支持自闭和标签
+  selfCloseEnabled: false,
   // 是否支持大写属性
+  uppercaseAttrEnabled: false,
   // 是否支持自定义标签
+  customTagEnabled: false,
+  // 是否解析 <script> 和 <template> 标签中的内容
   scriptingEnabled: true,
+  // 是否在 AST 节点中添加源代码位置信息
   sourceCodeLocationInfo: false,
+  // 指定用于构建 DOM 树的适配器
   treeAdapter: TreeAdapter,
   // 解析报错时调用的方法
   onParseError: null,
