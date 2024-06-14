9f88bb15af0c5de4da7c2c47e9cae19f7ab169b8
tenado <690784374@qq.com>
2024-06-14 16:19:24 +0800

    1

--- build/index.js
+++
@@ -1,4 +0,0 @@
-const { Parse }  = require("../src/index.js");
-let sourceCode = '<!DOCTYPE html><html><head></head><body>Hi there!</body></html>';
-const result = Parse(sourceCode);
-console.log("result", result)
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
-import exampleVite from "./example.vite.js";
-import libVite from "./lib.vite.js";
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const { VITE_CLI_TYPE } = env || {};
-  if(VITE_CLI_TYPE === "example") {
-    return exampleVite();
-  }
-  if(VITE_CLI_TYPE === "lib") {
-    return libVite();
-  }
-})
+import { defineConfig } from "vite";
+import { resolve } from "path";
+export default defineConfig(() => {
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.js"),
+        name: "htmlParsing",
+        fileName: "index",
+      },
+      outDir: "lib",
+      cssCodeSplit: false,
+      sourcemap: true,
+      minify: false,
+    },
+  };
+});
